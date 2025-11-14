eca61dc8fd2af15d1b436492ba88226e98db69ff
karlidean <karlidean11@gmail.com>
2025-11-14 15:49:58 -0600

    Changed python script name for p4.

--- src/analytics_project/etl_to_dw.py
+++ src/analytics_project/etl_to_dw.py
@@ -1,13 +1,14 @@
-import pandas as pd
-import sqlite3
 import pathlib
+import sqlite3
 import sys
 
+import pandas as pd
+
 # For local imports, temporarily add project root to sys.path
-# Note: this can be removed - our project uses a modern /src/ folder and __init__.py files 
-# To make local imports easier. 
+# Note: this can be removed - our project uses a modern /src/ folder and __init__.py files
+# To make local imports easier.
 # Adjust the paths and code to fit with this updated organization.
-# Questions: Ask them here in this project discussion and we can help. 
+# Questions: Ask them here in this project discussion and we can help.
 PROJECT_ROOT = pathlib.Path(__file__).resolve().parent.parent
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.append(str(PROJECT_ROOT))
@@ -16,6 +17,7 @@ if str(PROJECT_ROOT) not in sys.path:
 DW_DIR = pathlib.Path("data").joinpath("dw")
 DB_PATH = DW_DIR.joinpath("smart_sales.db")
 PREPARED_DATA_DIR = pathlib.Path("data").joinpath("prepared")
+
 
 def create_schema(cursor: sqlite3.Cursor) -> None:
     """Create tables in the data warehouse if they don't exist."""
@@ -30,7 +32,7 @@ def create_schema(cursor: sqlite3.Cursor) -> None:
               preferred_contact  TEXT
         )
     """)
-    
+
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS product (
               product_key     INTEGER PRIMARY KEY,     -- = ProductID
@@ -43,7 +45,7 @@ def create_schema(cursor: sqlite3.Cursor) -> None:
               supplier_name   TEXT
         )
     """)
-    
+
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS sale (
             sale_id INTEGER PRIMARY KEY,
@@ -56,23 +58,28 @@ def create_schema(cursor: sqlite3.Cursor) -> None:
         )
     """)
 
+
 def delete_existing_records(cursor: sqlite3.Cursor) -> None:
     """Delete all existing records from the customer, product, and sale tables."""
     cursor.execute("DELETE FROM customer")
     cursor.execute("DELETE FROM product")
     cursor.execute("DELETE FROM sale")
 
+
 def insert_customers(customers_df: pd.DataFrame, cursor: sqlite3.Cursor) -> None:
     """Insert customer data into the customer table."""
     customers_df.to_sql("customer", cursor.connection, if_exists="append", index=False)
+
 
 def insert_products(products_df: pd.DataFrame, cursor: sqlite3.Cursor) -> None:
     """Insert product data into the product table."""
     products_df.to_sql("product", cursor.connection, if_exists="append", index=False)
 
+
 def insert_sales(sales_df: pd.DataFrame, cursor: sqlite3.Cursor) -> None:
     """Insert sales data into the sales table."""
     sales_df.to_sql("sale", cursor.connection, if_exists="append", index=False)
+
 
 def load_data_to_db() -> None:
     try:
@@ -98,6 +105,7 @@ def load_data_to_db() -> None:
     finally:
         if conn:
             conn.close()
+
 
 if __name__ == "__main__":
     load_data_to_db()
